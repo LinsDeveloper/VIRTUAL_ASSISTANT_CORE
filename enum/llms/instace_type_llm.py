bee12ee5b8a5d4ef7f1b54115148c860eaa0d975
Pedro-H Lins <pedro.lins.wickos@outlook.com>
2025-05-20 00:48:52 -0300

    refactor: refatorando instancias de modelos

---
+++ enum/llms/instace_type_llm.py
@@ -0,0 +1,5 @@
+from enum import Enum
+
+class InstanceTypeLLM(Enum):
+    OLLAMA = 0
+    GROQ = 1
--- llms/main_llm.py
+++ llms/main_llm.py
@@ -1,23 +1,22 @@
 import os
+from enum.llms.instace_type_llm import InstanceTypeLLM
 from langchain.chat_models import ChatOpenAI
 from langchain.schema import SystemMessage, HumanMessage
 from dotenv import load_dotenv
+from llms.groq_llms import build_model_groq
+from llms.ollama import build_model_ollama
 
 load_dotenv()
 
-def build_model(instance_type=, model_id: str, temperature: float = 0.7) -> ChatOpenAI:
+def build_model(instance_type: str, model_id: str, temperature: float = 0.7) -> any:
     """
-    Build the Groq model with the specified parameters.
+    Build the model with the specified parameters.
     """
-    groq_api_key = os.getenv("GROQ_API_KEY")
-    if not groq_api_key:
-        raise ValueError("API key not found. Please set the GROQ_API_KEY environment variable.")
     
-    llm = ChatOpenAI(
-        openai_api_key=groq_api_key,
-        openai_api_base="https://api.groq.com/openai/v1",
-        model_name=model_id,
-        temperature=temperature,
-    )
-    
-    return llm
+    match instance_type:
+        case InstanceTypeLLM.OLLAMA:
+            return build_model_ollama(model_id, temperature)
+        case InstanceTypeLLM.GROQ:
+            return build_model_groq(model_id, temperature)
+        
+    raise ValueError(f"Unknown instance type: {instance_type}")
